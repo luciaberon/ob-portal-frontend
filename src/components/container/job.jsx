2ab42a31ae5e9eb1f809084ce513de9055398f67
Lucia <luciaberonwaingart@hotmail.com>
2021-11-02 16:21:30 -0300

    go to individual offer after clicking it

--- src/components/container/job.jsx
+++ src/components/container/job.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import Image from '../pure/company_logo';
 import Company from '../pure/company_name'
 import Position from '../pure/job_position';
@@ -13,10 +14,12 @@ import Button from '../pure/apply_button';
  */
 const Job = ({offer}) => {
    
-    const { urlImagen, nombre, empresa, salarioMaximo, salarioMinimo, tecnologias, fechaPublicacion, localidad } = offer || {};
+    const history = useHistory();
+
+    const { urlImagen, id, nombre, empresa, salarioMaximo, salarioMinimo, tecnologias, fechaPublicacion, localidad } = offer || {};
     
     return (
-        <div className="collapsible select-none max-w-xl lg:max-w-screen-xl mx-auto md:max-w-3xl p-5">
+        <div onClick={() => history.push(`/ofertas/${id}`)} className="collapsible select-none max-w-xl lg:max-w-screen-xl mx-auto md:max-w-3xl p-5">
             <div className="max-w-lg hover:bg-gray-100 cursor-pointer lg:max-w-screen-lg flex bg justify-between rounded-xl border overflow-hidden md:max-w-2xl p-5">
 
                 <Image image={urlImagen.length > 0 ? urlImagen : "https://remoteok.io/assets/img/jobs/376af38d16bf762263aef1f70ed885ef1633445669.png?1633445670"}/>
